f6ce43417b9e14c68dff5d26f8b2ed1682169b88
marcolilli <mail@marcolilli.com>
2024-04-08 20:36:27 +0200

    Add es5 examples

---
+++ es-5-code-examples/searching-values.js
@@ -0,0 +1,14 @@
+const people = [
+    { name: 'John Doe', age: 30 },
+    { name: 'Jane Smith', age: 25 },
+    { name: 'Emily Davis', age: 35 },
+    { name: 'Jannet Jones', age: 22 }
+];
+
+// Log the result after each task:
+
+// 1. Sort people by age...
+
+// 2. Find a person whose name starts with 'Jan'...
+
+// 3. Joining all names together to form a single string separated by a comma...
---
+++ es-5-code-examples/working-with-objects.js
@@ -0,0 +1,16 @@
+const library = [
+    {title: 'The Road Ahead', author: 'Bill Gates', year: 1995, bestseller: true},
+    {title: 'Walter Isaacson', author: 'Steve Jobs', year: 2011, bestseller: true},
+    {title: 'Mockingjay', author: 'Suzanne Collins', year: 2010, bestseller: false},
+    {title: 'The Hunger Games', author: 'Suzanne Collins', year: 2008, bestseller: true}
+]
+
+// Log the result after each task:
+
+// List each author (without duplicates)...
+
+// List all book titles published after 2000...
+
+// List all property names of the first book separated by a comma (expected output: 'title, author, year')...
+
+// List all bestseller book titles...
