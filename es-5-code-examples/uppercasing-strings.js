1c96a15bf6ea4c1ddbe8a75750d1f05fb1481902
marcolilli <mail@marcolilli.com>
2024-04-08 20:16:15 +0200

    Add es5 example - uppercasing string

---
+++ es-5-code-examples/uppercasing-strings.js
@@ -0,0 +1,12 @@
+var fruits = ['apple', 'banana', 'cherry'];
+
+var uppercaseFruits = [];
+for (var i = 0; i < fruits.length; i++) {
+    uppercaseFruits.push(fruits[i].toUpperCase());
+}
+
+for (var j = 0; j < fruits.length; j++) {
+    console.log('Fruit: ' + fruits[i]);
+}
+
+console.log(uppercaseFruits);
